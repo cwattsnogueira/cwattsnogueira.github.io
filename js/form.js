94515c02b608c3aa1eed1cd08807c2ea5e6cb789
Carllos Watts-Nogueira <carlloswattsnogueira@gmail.com>
2025-09-10 14:10:09 -0400

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,38 +1,16 @@
 const form = () => {
-  const contactForm = document.querySelector(".contactForm"),
-    responseMessage = document.querySelector(".response");
+  const contactForm = document.querySelector("#contactForm");
+  const responseMessage = document.querySelector(".response");
 
-  contactForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const formData = new FormData(form);
+  if (!contactForm) return;
+
+  contactForm.addEventListener("submit", () => {
     responseMessage.classList.add("open");
-    responseMessage.textContent = "Please wait...";
-
-    async function getData() {
-      try {
-        const response = await fetch("mail.php", {
-          method: "POST",
-          body: formData,
-        });
-        if (!response.ok) {
-          responseMessage.textContent = result;
-        }
-
-        const result = await response.text();
-        responseMessage.textContent = result;
-      } catch (error) {
-        console.error(error.message);
-      }
-    }
-
-    getData()
-      .then(
-        setTimeout(() => {
-          responseMessage.classList.remove("open");
-        }, 3000)
-      )
-      .finally(form.reset());
+    responseMessage.textContent = "Sending message...";
+    setTimeout(() => {
+      responseMessage.classList.remove("open");
+    }, 3000);
   });
 };
+
 export default form;
