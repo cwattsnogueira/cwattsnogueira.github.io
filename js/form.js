fd883267ae7caf2eaf4b140ad8276b9f69c3ba27
Carllos Watts-Nogueira <carlloswattsnogueira@gmail.com>
2025-10-11 17:34:13 -0400

    Update form.js

--- js/form.js
+++ js/form.js
@@ -4,9 +4,13 @@ const form = () => {
 
   if (!contactForm) return;
 
-  contactForm.addEventListener("submit", () => {
+  contactForm.addEventListener("submit", (e) => {
+    // show sending feedback
     responseMessage.classList.add("open");
     responseMessage.textContent = "Sending message...";
+
+    // allow the form to submit to formsubmit.co normally
+    // hide message after 3s
     setTimeout(() => {
       responseMessage.classList.remove("open");
     }, 3000);
